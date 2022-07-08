6815c8069087c22e7e6a39f2569a7ab325b067e1
hayden <haydenburger43@gmail.com>
2022-07-07 19:03:07 -0500

    added timer button and html

---
+++ app.js
@@ -0,0 +1,14 @@
+const startBtn = document.querySelector('#startBtn')
+const timer = document.querySelector("#Timer")
+let time = 75;
+const questions = [{question: 'What is today?', answer: 'thursday'},{}]
+
+startBtn.addEventListener("click", function() {
+//add timer
+    
+    setInterval(function(){
+        time--
+        timer.textContent = time;
+    }, 1000)
+
+})
