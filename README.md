d75c63e2a240d8239719089612be43af7ad96d15
hayden <haydenburger43@gmail.com>
2022-08-10 17:03:15 -0500

    added readme and const to js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # code-quiz
+
+## quiz that sets timer and puts questions and answers onto the page to quiz you
+
+##created by Hayden Carr and Brian Weinthal
--- app.js
+++ app.js
@@ -1,7 +1,51 @@
 const startBtn = document.querySelector('#startBtn')
 const timer = document.querySelector("#Timer")
+const quiz= document.getElementById('quiz')
+const questions= document.getElementById('questions')
+const answerA = document.getElementById('answer.a')
+const answerB = document.getElementById('answer.b')
+const answerC = document.getElementById('answer.c')
+const answerD = document.getElementById('answer.d')
+
 let time = 75;
-const questions = [{question: 'What is today?', answer: 'thursday'},{}]
+const questionsArr = [
+    {
+       question: "what does js stand for",
+       options: ["Javascript", "java", "juicy fruit", "all of the above"],
+       answer: "Javascript",
+       id: 1
+    },
+
+    {
+        question: "what does cs stand for",
+        options: ["Javascript", "computer science", "coffee supreme", "all of the above"],
+        answer: "computer science",
+        id: 2
+     },
+
+     {
+        question: "what is css used for",
+        options: ["designing the webpage", "I dont know", "juicy fruit", "all of the above"],
+        answer: "designing the webpage",
+        id: 3
+     },
+
+     {
+        question: "what do you use for an array",
+        options: ["{}", "[]", "''", "all of the above"],
+        answer: "[]",
+        id:4
+     },
+
+     {
+        question: "where do you remotley store data",
+        options: ["Javascript", "github", "juicy fruit", "all of the above"],
+        answer: "github",
+        id: 5
+     },
+
+
+   ];
 
 startBtn.addEventListener("click", function() {
 //add timer
